3093b6950b902500a0a40985604a5aa6668fab58
이시온 <sion9199@gmail.com>
2024-12-24 11:17:40 +0900

    Delete

--- day2/dao/DBInfo.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-public class DBInfo {	
-	static final public String driver = "";
-	static final public String url = "";
-	static final public String account = "";
-	static final public String password = "";
-
-}
