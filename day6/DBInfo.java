f746f63df5d4ae989fadee711326ae5b06eeb159
이시온 <sion9199@gmail.com>
2024-12-24 11:19:51 +0900

    Delete day6/DBInfo.java

--- day6/DBInfo.java
+++
@@ -1,9 +0,0 @@
-package mvc;
-
-public class DBInfo {	
-	static final public String driver = "";
-	static final public String url = "";
-	static final public String account = "";
-	static final public String password = "";
-
-}
